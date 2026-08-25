3f16ab6026cc0bc5ae9e253a325b7b6008450386
Ziaullah Khan <ziaullahkhan.se@gmail.com>
2026-08-25 20:07:17 +0500

    added

---
+++ string_to_number.py
@@ -0,0 +1,15 @@
+def string_to_number(s):
+    s=str(s)
+    convert=int(s)
+    return s
+
+converter1=string_to_number("1234")
+converter2=string_to_number("605")
+converter3=string_to_number("1405")
+converter4=string_to_number("-7")
+print("Convert1: ",converter1)
+print("Convert2: ",converter2)
+print("Convert3: ",converter3)
+print("Convert4: ",converter4)
+
+
